d36f750cad35358ed5016b02dbebeb427ab9bb8f
dancruznyc <dancruznyc@gmail.com>
2022-05-20 13:48:32 -0400

    add RRD" git push

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,39 @@ import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ProductList from "./pages/ProductList";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 //import ProductList from "./pages/ProductList";
 
 const App = () => {
-  return <Cart />;
+  const user = true;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
